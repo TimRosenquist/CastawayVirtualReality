c8f9e8103455bfc250d29f33e98b9bf395c5fdaf
ergu5011 <ergu5011@gmail.com>
2022-12-17 23:10:11 +0100

    added stuff

--- Assets/CastawayVirtualReality/Scripts/DisplayOutOfRange.cs
+++ Assets/CastawayVirtualReality/Scripts/DisplayOutOfRange.cs
@@ -15,16 +15,19 @@ namespace GameEventSystem
         {
             _renderer = GetComponent<Renderer>();
             _renderer.enabled = true;
+            // Sets the map screen to the drone camera at start
             _renderer.sharedMaterial = _material[0];
         }
 
         public void OutOfRange()
         {
+            // Sets the map screen to "not in service" when the right condition is fulfilled
             _renderer.sharedMaterial = _material[1];
         }
 
         public void InToRange()
         {
+            // Sets the map screen to the drone camera when the right condition is fullfilled
             _renderer.sharedMaterial = _material[0];
         }
     }
