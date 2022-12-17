43bde2286e9fd8806aecf1300fa8cc106589ba64
ergu5011 <ergu5011@gmail.com>
2022-12-17 17:58:39 +0100

    added raftbuilding and credits

--- Assets/CastawayVirtualReality/Scripts/MaterialsManager.cs
+++ Assets/CastawayVirtualReality/Scripts/MaterialsManager.cs
@@ -9,38 +9,91 @@ namespace GameEventSystem
         private int logCount = 0;
         private int ropeCount = 0;
 
+        [SerializeField]
+        private GameObject raft;
+        private Animator raftAnimator;
+        public GameEvent changeRaftmaterial;
+
+        [SerializeField]
+        private AudioSource raftSource;
+        [SerializeField]
+        private AudioClip raftClip;
+        
+        [SerializeField]
+        private AudioSource deniedSource;
+        [SerializeField]
+        private AudioClip deniedClip;
+        [SerializeField]
+        private GameObject raftButton;
+
+        [SerializeField]
+        private GameObject credits;
+
+        [SerializeField]
+        private GameObject log_1;
+        [SerializeField]
+        private GameObject log_2;
+        [SerializeField]
+        private GameObject log_3; 
+        [SerializeField]
+        private GameObject log_4; 
+        [SerializeField]
+        private GameObject log_5; 
+        [SerializeField]
+        private GameObject rope_1; 
+        [SerializeField]
+        private GameObject rope_2;
+
+        private void Start()
+        {
+            raftAnimator = raft.GetComponent<Animator>();
+            raftSource = raft.GetComponent<AudioSource>();
+            deniedSource = raftButton.GetComponent<AudioSource>();
+            credits.SetActive(false);
+        }
+
         public void LogCountIncrease()
         {
             logCount++;
-            //Debug.Log(logCount);
-
-            if (logCount == 5)
-            {
-                Debug.Log("you have 5 logs");
-            }
         }
 
         public void LogCountDecrease()
         {
             logCount--;
-            //Debug.Log(logCount);
         }
 
         public void RopeCountIncrease()
         {
             ropeCount++;
-            //Debug.Log(ropeCount);
-
-            if (ropeCount == 2)
-            {
-                Debug.Log("you have 2 ropes");
-            }
         }
 
         public void RopeCountDecrease()
         {
             ropeCount--;
-            //Debug.Log(ropeCount);
+        }
+
+        public void MaterialsCheck()
+        {
+            if (logCount == 1 && ropeCount == 0)
+            {
+                changeRaftmaterial.TriggerEvent();
+                raftSource.PlayOneShot(raftClip);
+                raftAnimator.SetTrigger("RaftPush");
+
+                log_1.SetActive(false);
+                log_2.SetActive(false);
+                log_3.SetActive(false);
+                log_4.SetActive(false);
+                log_5.SetActive(false);
+                rope_1.SetActive(false);
+                rope_2.SetActive(false);
+
+                credits.SetActive(true);
+            }
+            else
+            {
+                deniedSource.PlayOneShot(deniedClip);
+            }
         }
     }
 }
