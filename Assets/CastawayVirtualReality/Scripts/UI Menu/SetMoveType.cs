3351da1038b3cdd7d7ee507a8c1afc587aecf728
Jesper Elmtoft <lars.elmtoft@yh.nackademin.se>
2022-12-18 13:19:58 +0100

    Commented cod efor the UI

--- Assets/CastawayVirtualReality/Scripts/UI Menu/SetMoveType.cs
+++ Assets/CastawayVirtualReality/Scripts/UI Menu/SetMoveType.cs
@@ -7,9 +7,11 @@ namespace UIMenu
 {
     public class SetMoveType : MonoBehaviour
     {
-        public ActionBasedContinuousMoveProvider continuesMove;
-        public TeleportationProvider teleportMove;
+        public ActionBasedContinuousMoveProvider continuesMove; // Gives you access to the Continous Movement function for the VR and gives it a variable for later use in the code
+        public TeleportationProvider teleportMove; // Gives you access to the Teleportation function for the VR and gives it a variable for later use in the code
 
+        // In this function we use the earlier declared variables to be able to switch between the two movement types, depending if the player 
+        // wants to move around with Continous Movement or by using Teleportation based on index numbers 0 and 1
         public void SetTypeFromIndex(int index)
         {
             if (index == 0)
